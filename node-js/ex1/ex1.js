fcdd598bf7066c19076ebec905c559ae6df4bdb0
IDrissAitHafid <31975500+IDrissAitHafid@users.noreply.github.com>
2018-11-17 15:42:20 +0100

    node-js->ex1: Done!

--- node-js/ex1/ex1.js
+++ node-js/ex1/ex1.js
@@ -30,6 +30,53 @@ let outputNotPalindromes = [];
 // please start your implementation below:
 //
 
+var palindromeFinder = {
+
+    find: () => {
+        let l, j = 0
+        for (var i = 0; i < DATA_SOURCE.length; i++) {
+            if (typeof (DATA_SOURCE[i]) == "string") {
+                j = 0
+                toBeCompared = DATA_SOURCE[i].toLowerCase()
+                toBeCompared = toBeCompared.replace(/[.,\/#!?$%\^&\*;:{}=\-_`~()\s]/g, '')
+                l = toBeCompared.length
+                while (j < Math.floor(l / 2)) {
+                    if (toBeCompared[j] != toBeCompared[l - 1 - j]) {
+                        break;
+                    } else {
+                        j++;
+                    }
+                }
+                if (j >= Math.floor(l / 2)) {
+                    outputPalindromes.push(DATA_SOURCE[i])
+                }
+            }
+        }
+
+        return outputPalindromes;
+    },
+    findOther: () => {
+        let l, j = 0
+        for (var i = 0; i < DATA_SOURCE.length; i++) {
+            if (typeof (DATA_SOURCE[i]) == "string") {
+                j = 0
+                toBeCompared = DATA_SOURCE[i].toLowerCase()
+                toBeCompared = toBeCompared.replace(/[.,\/#!?$%\^&\*;:{}=\-_`~()\s]/g, '')
+                l = toBeCompared.length
+                while (j < Math.floor(l / 2)) {
+                    if (toBeCompared[j] != toBeCompared[l - 1 - j]) {
+                        outputNotPalindromes.push(DATA_SOURCE[i])
+                        break;
+                    } else {
+                        j++;
+                    }
+                }
+            }
+        }
+
+        return outputNotPalindromes;
+    }
+}
 
 //
 // end of your implementation
